2aa22334227683bd60c080ed31c6ea805212d5aa
Je11yyy <pidlisnyackkiril@gmail.com>
2025-12-17 17:53:14 +0200

    main

--- LibraryApp/app.py
+++ LibraryApp/app.py
@@ -2,16 +2,20 @@ from storage import Storage_libr, Storage_users
 from models import Book, Reader, Types
 from library_core import Library
 
-# Создание хранилища книг
-storage_books = Storage_libr('library_2.txt')
-library = Library(storage_books)
+def main():
+    # Создание хранилища книг
+    storage_books = Storage_libr('library_2.txt')
+    library = Library(storage_books)
 
-# Создание хранилища пользователей
-storage_users = Storage_users()
+    # Создание хранилища пользователей
+    storage_users = Storage_users()
 
-# Создание пользователя
-user = Reader('Jhon Patrik', storage_users)
+    # Создание пользователя
+    user = Reader('Jhon Patrik', storage_users)
 
-# Пример покупки книги
-result = user.buy('harry potter 1', 2, library)
-print(result)
+    # Пример покупки книги
+    result = user.buy('harry potter 1', 2, library)
+    print(result)
+
+if __name__ == "__main__":
+    main()
